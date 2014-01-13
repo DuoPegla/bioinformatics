85a0790954622072325ae39557196382be610ee8
joshko <josip.sarlija@gmail.com>
2014-01-13 18:28:25 +0100

    Fixed output

--- Main.py
+++ Main.py
@@ -2,30 +2,36 @@ from MSA import MSA
 from InputOutput import InputData, OutputData
 from Hypercube import Hypercube
 import time
+import resource
+
+def memory_usage():
+    return resource.getrusage(resource.RUSAGE_SELF).ru_maxrss /float(1000)
 
 print ("INFO: Starting Multiple Sequence Alignment.")
-execution_times = dict()
+performance = dict()
 start_time = time.clock()
 input_data = InputData("./input1.txt")
 end_time = time.clock()
-execution_times["input"] = end_time - start_time
+performance["input"] = (end_time - start_time, memory_usage())
 
 start_time = time.clock()
 hypercube = Hypercube(input_data.sequences)
 end_time = time.clock()
-execution_times["hypercube"] = end_time - start_time
+performance["hcube"] = (end_time - start_time, memory_usage())
 
 
 msa = MSA(hypercube)
 start_time = time.clock()
 msa.align()
 end_time = time.clock()
-execution_times["MSA"] = end_time - start_time
+performance["MSA"] = (end_time - start_time, memory_usage())
 
 for output in msa.output:
     print (output)
 
 output_data = OutputData("./output.txt", msa.output)
 
-print ("INFO: Execution time [s]: {0}".format(execution_times))
+print ("INFO: Performance: (Execution time [s], Memory usage [MB])")
+for p in performance:
+    print ("\t{0}: {1} s, {2} MB".format(p, performance[p][0], performance[p][1]))
 print ("INFO: Done.")
