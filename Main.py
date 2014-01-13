fd70ca0e8b8294fe8a9a49790eb6657a1628336b
joshko <joshko@joshko-virtual-machine>
2014-01-13 17:00:16 +0100

    Changed file paths to run on Linux.

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ import time
 print ("INFO: Starting Multiple Sequence Alignment.")
 execution_times = dict()
 start_time = time.clock()
-input_data = InputData(".\input.txt")
+input_data = InputData("./input1.txt")
 end_time = time.clock()
 execution_times["input"] = end_time - start_time
 
@@ -25,7 +25,7 @@ execution_times["MSA"] = end_time - start_time
 for output in msa.output:
     print (output)
 
-output_data = OutputData(".\output.txt", msa.output)
+output_data = OutputData("./output.txt", msa.output)
 
 print ("INFO: Execution time [s]: {0}".format(execution_times))
 print ("INFO: Done.")
